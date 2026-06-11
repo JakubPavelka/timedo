d7e5a1b4425bffec8b26ebed129cb6ae140eab69
Jakub Pavelka <pavelka.kuba@hotmail.cz>
2026-06-11 10:42:19 +0200

    added packages + fixed prisma db connection

--- app/backend/src/db/db.ts
+++ app/backend/src/db/db.ts
@@ -1,6 +1,20 @@
 import { PrismaClient } from "../generated/prisma/client.js";
 import { PrismaPg } from "@prisma/adapter-pg";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
-const prisma = new PrismaClient({ adapter });
-export default prisma;
+const prisma = new PrismaClient({
+  adapter,
+  log:
+    process.env.NODE_ENV === "development"
+      ? ["query", "error", "warn"]
+      : ["error"],
+});
+
+const disconnectDB = async () => {
+  await prisma.$disconnect();
+};
+
+export { prisma, disconnectDB };
