c7e93ebaf33a261022e27ae0a03b18d9ff19afe2
Jakub Pavelka <pavelka.kuba@hotmail.cz>
2026-06-15 10:06:05 +0200

    Added fonts and lucide react icons

--- app/frontend/src/main.tsx
+++ app/frontend/src/main.tsx
@@ -1,5 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "@fontsource/hanken-grotesk/400.css";
+import "@fontsource/hanken-grotesk/500.css";
+import "@fontsource/hanken-grotesk/600.css";
+import "@fontsource/hanken-grotesk/700.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
