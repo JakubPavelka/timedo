927afdb7eaa96ec24b23cf12bfac5ee5403e15d3
Jakub Pavelka <pavelka.kuba@hotmail.cz>
2026-06-18 20:52:10 +0200

    Added spinner to button when loading

--- app/frontend/src/components/ui/Button/Button.tsx
+++ app/frontend/src/components/ui/Button/Button.tsx
@@ -2,17 +2,20 @@ import type { ButtonHTMLAttributes } from "react";
 import styles from "./Button.module.scss";
 import { ChevronRight } from "lucide-react";
 import clsx from "clsx";
+import { Spinner } from "../Spinner/Spinner";
 
 type ButtonVariant = "primary" | "secondary";
 
 type ButtonProps = {
   haveRightArrow?: boolean;
   variant?: ButtonVariant;
+  isLoading?: boolean;
 } & ButtonHTMLAttributes<HTMLButtonElement>;
 
 export const Button = ({
   variant = "primary",
   haveRightArrow,
+  isLoading,
   children,
   type,
   ...rest
@@ -21,15 +24,20 @@ export const Button = ({
     <button
       className={clsx(styles.Button, styles[`Button--${variant}`])}
       type={type ?? "button"}
+      disabled={isLoading}
       {...rest}
     >
       <span className={styles.Button__text}>{children}</span>
-      {haveRightArrow && (
-        <ChevronRight
-          className={styles.Button__chevronRight}
-          width={16}
-          height={16}
-        />
+      {isLoading ? (
+        <Spinner size="sm" className={styles.Button__spinner} />
+      ) : (
+        haveRightArrow && (
+          <ChevronRight
+            className={styles.Button__chevronRight}
+            width={16}
+            height={16}
+          />
+        )
       )}
     </button>
   );
