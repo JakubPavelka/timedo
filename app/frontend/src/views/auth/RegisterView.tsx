5756e71b53a89d7667f17df7e00998ed688f76b7
Jakub Pavelka <pavelka.kuba@hotmail.cz>
2026-06-18 20:17:49 +0200

    WIP Register view

--- app/frontend/src/views/auth/RegisterView.tsx
+++ app/frontend/src/views/auth/RegisterView.tsx
@@ -3,20 +3,36 @@ import { ThemeSwitch } from "../../components/ui/ThemeSwitch/ThemeSwitch";
 import { useTheme } from "@/hooks/useTheme";
 import { AuthCard } from "@/components/features/Auth/AuthCard/AuthCard";
 import { useTranslation, Trans } from "react-i18next";
-import { Link } from "@tanstack/react-router";
+import { Link, useNavigate } from "@tanstack/react-router";
 import LogoBlue from "@/assets/images/logoBlue.svg?react";
 import LogoBlueDarkTheme from "@/assets/images/logoBlueDarkTheme.svg?react";
 import styles from "./AuthStyles.module.scss";
 import TimerCard from "@/assets/images/timerCard.png";
+import { useRegister } from "@/hooks/api/useAuth";
+import type { RegisterData } from "@timedo/shared/src/schemas/authSchema";
+import axios from "axios";
 
 export const RegisterView = () => {
   const { t } = useTranslation();
-
+  const { mutate: register, error } = useRegister();
   const theme = useTheme((s) => s.theme);
-  console.log("theme", theme);
+  const navigate = useNavigate();
+
+  const registerHandler = (data: RegisterData) => {
+    register(
+      {
+        firstName: data.firstName,
+        lastName: data.lastName,
+        email: data.email,
+        password: data.password,
+      },
+      { onSuccess: () => navigate({ to: "/dashboard", replace: true }) },
+    );
+  };
 
   return (
     <div className={styles.AuthView}>
+      {/* LEFT SIDE */}
       <div className={styles.AuthView__leftSide}>
         <div className={styles.AuthView__logoWrapper}>
           {theme === "light" ? (
@@ -29,7 +45,14 @@ export const RegisterView = () => {
         <div className={styles.AuthView__alignCenter}>
           <div className={styles.AuthView__leftContent}>
             <AuthCard />
-            <RegisterForm />
+            <RegisterForm onSubmit={registerHandler} />
+            {error && (
+              <p className={styles.AuthView__errorMessage}>
+                {axios.isAxiosError(error)
+                  ? t(`BackendErrors.${error.response?.data?.code}`)
+                  : error.message}
+              </p>
+            )}
             <p className={styles.AuthView__alreadyHaveAccount}>
               <Trans
                 i18nKey="RegisterForm.alreadyHaveAccount"
@@ -41,6 +64,8 @@ export const RegisterView = () => {
           </div>
         </div>
       </div>
+
+      {/* RIGHT SIDE */}
       <div className={styles.AuthView__rightSide}>
         <img src={TimerCard} />
         <div className={styles.AuthView__rightContent}>
