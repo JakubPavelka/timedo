d9d634c42c72120d2f9de19cbdb507e9c8db6e2f
Jakub Pavelka <pavelka.kuba@hotmail.cz>
2026-06-19 12:42:25 +0200

    Added LoginForm and LoginView

--- app/frontend/src/views/auth/LoginView.tsx
+++ app/frontend/src/views/auth/LoginView.tsx
@@ -1,3 +1,61 @@
+import { AuthCard } from "@/components/features/Auth/AuthCard/AuthCard";
+import { useTranslation, Trans } from "react-i18next";
+import { Link, useNavigate } from "@tanstack/react-router";
+import { useLogin } from "@/hooks/api/useAuth";
+import type { LoginData } from "@timedo/shared/src/schemas/authSchema";
+import { ApiAuthError } from "@/api/auth/auth.api";
+import { AuthRightSide } from "@/components/features/Auth/AuthRightSide/AuthRightSide";
+import { AuthHeaderLogo } from "@/components/features/Auth/AuthHeaderLogo/AuthHeaderLogo";
+import { LoginForm } from "@/components/features/forms/AuthForm/LoginForm";
+import styles from "./AuthStyles.module.scss";
+
 export const LoginView = () => {
-  return <div>Login</div>;
+  const { t } = useTranslation();
+  const { mutate: login, error, isPending } = useLogin();
+  const navigate = useNavigate();
+
+  const loginHandler = (data: LoginData) => {
+    login(
+      {
+        email: data.email,
+        password: data.password,
+      },
+      { onSuccess: () => navigate({ to: "/dashboard", replace: true }) },
+    );
+  };
+
+  return (
+    <div className={styles.AuthView}>
+      {/* LEFT SIDE */}
+      <div className={styles.AuthView__leftSide}>
+        <AuthHeaderLogo />
+        <div className={styles.AuthView__alignCenter}>
+          <div className={styles.AuthView__leftContent}>
+            <AuthCard />
+            <LoginForm onSubmit={loginHandler} isLoading={isPending} />
+            {error && (
+              <p className={styles.AuthView__errorMessage}>
+                {error instanceof ApiAuthError
+                  ? t(`BackendErrors.${error.code}`)
+                  : error.message}
+              </p>
+            )}
+            <p className={styles.AuthView__alreadyHaveAccount}>
+              <Trans
+                i18nKey="LoginForm.dontHaveAccount"
+                components={{
+                  link1: <Link to="/register" />,
+                }}
+              />
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* RIGHT SIDE */}
+      <div className={styles.AuthView__rightSide}>
+        <AuthRightSide />
+      </div>
+    </div>
+  );
 };
