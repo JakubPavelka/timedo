661ed6f6ee43cc5f28ae4c7b070651d8351b768f
Jakub Pavelka <pavelka.kuba@hotmail.cz>
2026-06-24 16:39:51 +0200

    Renamed useAuthStore to authStore

--- app/frontend/src/hooks/api/useAuth.ts
+++ app/frontend/src/hooks/api/useAuth.ts
@@ -1,5 +1,5 @@
 import { authApi } from '@/api/auth/auth.api';
-import { useAuthStore } from '@/store/useAuthStore';
+import { useAuthStore } from '@/store/authStore';
 import { useMutation, useQuery } from '@tanstack/react-query';
 import { useRouter } from '@tanstack/react-router';
 
