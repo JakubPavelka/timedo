d7189b8ac9916fc79c33562f83345d32d77f963e
Jakub Pavelka <pavelka.kuba@hotmail.cz>
2026-06-29 19:16:06 +0200

    Added ProfileForm form

---
+++ app/frontend/src/components/features/forms/ProfileForm/ProfileForm.tsx
@@ -0,0 +1,113 @@
+import { Input } from '@/components/ui/Input/Input';
+import { useTranslation } from 'react-i18next';
+import { useForm, Controller } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+    ProfileSchema,
+    type ProfileData,
+} from '@/../../packages/shared/src/schemas/profileSchema';
+import { useAuthStore } from '@/store/authStore';
+import { Button } from '@/components/ui/Button/Button';
+import { Check } from 'lucide-react';
+import styles from './ProfileForm.module.scss';
+
+type ProfileForm = {
+    isEditing: boolean;
+    onSubmit: (data: ProfileData) => void;
+};
+
+export const ProfileForm = (props: ProfileForm) => {
+    const { t } = useTranslation();
+    const { user } = useAuthStore();
+    const {
+        control,
+        handleSubmit,
+        formState: { errors, isDirty },
+    } = useForm<ProfileData>({
+        mode: 'onSubmit',
+        resolver: zodResolver(ProfileSchema),
+        defaultValues: {
+            firstName: user?.firstName,
+            lastName: user?.lastName ?? '',
+        },
+    });
+
+    const onSubmitHandler = handleSubmit((data) => props.onSubmit(data));
+
+    return (
+        <form className={styles.ProfileForm} onSubmit={onSubmitHandler}>
+            <div className={styles.ProfileForm__inputsWrapper}>
+                <div className={styles.ProfileForm__nameWrapper}>
+                    <div className={styles.ProfileForm__labelWrapper}>
+                        <label htmlFor={'firstName'}>
+                            {t('Profile.PersonalInfo.firstName')}
+                        </label>
+                        <Controller
+                            name={'firstName'}
+                            control={control}
+                            render={({ field: { onChange, value } }) => (
+                                <Input
+                                    id={'firstName'}
+                                    onChange={onChange}
+                                    value={value}
+                                    disabled={!props.isEditing}
+                                />
+                            )}
+                        />
+                        {errors.firstName && (
+                            <p className={styles.ProfileForm__errorText}>
+                                {errors.firstName.message}
+                            </p>
+                        )}
+                    </div>
+                    <div className={styles.ProfileForm__labelWrapper}>
+                        <label htmlFor={'lastName'}>
+                            {t('Profile.PersonalInfo.lastName')}
+                        </label>
+                        <Controller
+                            name={'lastName'}
+                            control={control}
+                            render={({ field: { onChange, value } }) => (
+                                <Input
+                                    id={'lastName'}
+                                    onChange={onChange}
+                                    value={value}
+                                    disabled={!props.isEditing}
+                                />
+                            )}
+                        />
+                        {errors.lastName && (
+                            <p className={styles.ProfileForm__errorText}>
+                                {errors.lastName.message}
+                            </p>
+                        )}
+                    </div>
+                </div>
+                <div className={styles.ProfileForm__labelWrapper}>
+                    <label htmlFor={'email'}>
+                        {t('Profile.PersonalInfo.email')}
+                    </label>
+                    <Input
+                        id={'email'}
+                        value={user?.email}
+                        type={'email'}
+                        autoComplete={'email'}
+                        disabled
+                    />
+                </div>
+            </div>
+            {props.isEditing && (
+                <Button
+                    className={styles.ProfileForm__submitButton}
+                    type={'submit'}
+                    disabled={!isDirty}
+                >
+                    <span className={styles.ProfileForm__submitButtonText}>
+                        <Check width={16} height={16} />
+                        <span>{t('General.saveChanges')}</span>
+                    </span>
+                </Button>
+            )}
+        </form>
+    );
+};
