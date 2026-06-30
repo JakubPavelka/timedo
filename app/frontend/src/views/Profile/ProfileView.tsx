38ac33bd054b0c502520d83c025cfed1fac7c3ba
Jakub Pavelka <pavelka.kuba@hotmail.cz>
2026-06-30 19:52:08 +0200

    Added all profile components to ProfileView

--- app/frontend/src/views/Profile/ProfileView.tsx
+++ app/frontend/src/views/Profile/ProfileView.tsx
@@ -1,7 +1,22 @@
 import { ProfileHeader } from '@/components/features/Profile/ProfileHeader/ProfileHeader';
+import { ProfilePersonalInfo } from '@/components/features/Profile/ProfilePersonalInfo/ProfilePersonalInfo';
+import { ProfileAppPreferences } from '@/components/features/Profile/ProfileAppPreferences/ProfileAppPreferences';
+import { ProfileConnectedAccounts } from '@/components/features/Profile/ProfileConnectedAccounts/ProfileConnectedAccounts';
+import { ProfileSecurity } from '@/components/features/Profile/ProfileSecurity/ProfileSecurity';
+import styles from './ProfileView.module.scss';
 
 export const ProfileView = () => (
-    <div>
+    <div className={styles.ProfileView}>
         <ProfileHeader />
+        <div className={styles.ProfileView__flexWrapper}>
+            <div className={styles.ProfileView__leftSide}>
+                <ProfilePersonalInfo />
+                <ProfileAppPreferences />
+            </div>
+            <div className={styles.ProfileView__rightSide}>
+                <ProfileConnectedAccounts />
+                <ProfileSecurity />
+            </div>
+        </div>
     </div>
 );
