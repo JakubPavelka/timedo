4a654a2abe4b09e6095fa203a540ef5f44a32b31
Jakub Pavelka <pavelka.kuba@hotmail.cz>
2026-07-16 22:04:40 +0200

    Added ratelimit middleware to backend

--- app/backend/src/routes/userRoutes.ts
+++ app/backend/src/routes/userRoutes.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import { protect } from '../middleware/protect.js';
 import { deleteUser } from '../controllers/userController.js';
+import { deleteMeLimiter } from '../middleware/rateLimiters.js';
 
 const router = express.Router();
 
-router.delete('/me', protect, deleteUser);
+router.delete('/me', deleteMeLimiter, protect, deleteUser);
 
 export default router;
