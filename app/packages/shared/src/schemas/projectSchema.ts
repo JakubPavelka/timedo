c0a090a02c800d979cbb2eccb819bc924ac0c456
Jakub Pavelka <pavelka.kuba@hotmail.cz>
2026-07-21 20:14:24 +0200

    Updated projectSchema

--- app/packages/shared/src/schemas/projectSchema.ts
+++ app/packages/shared/src/schemas/projectSchema.ts
@@ -5,7 +5,9 @@ export const ProjectSchema = z.object({
         .string()
         .min(1, 'Validation.labelMin')
         .max(50, 'Validation.labelMax'),
-    color: z.string(),
+    color: z
+        .string()
+        .regex(/^#[0-9A-Fa-f]{6}$/, 'Validation.colorFormat'),
 });
 
 export type ProjectData = z.infer<typeof ProjectSchema>;
