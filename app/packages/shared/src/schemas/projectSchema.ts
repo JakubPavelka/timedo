827456e4f6943c46948396c8392e844fa64ba318
Jakub Pavelka <pavelka.kuba@hotmail.cz>
2026-07-21 11:50:48 +0200

    Added validation for label

--- app/packages/shared/src/schemas/projectSchema.ts
+++ app/packages/shared/src/schemas/projectSchema.ts
@@ -1,7 +1,10 @@
 import { z } from 'zod';
 
 export const ProjectSchema = z.object({
-    label: z.string(),
+    label: z
+        .string()
+        .min(1, 'Validation.labelMin')
+        .max(50, 'Validation.labelMax'),
     color: z.string(),
 });
 
