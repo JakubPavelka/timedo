ec85f598ab8bf51934dc3b9941e42b10d2eec90c
Jakub Pavelka <pavelka.kuba@hotmail.cz>
2026-07-29 11:49:08 +0200

    Added linkSchema

---
+++ app/packages/shared/src/schemas/linkSchema.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod';
+
+export const LinkBaseSchema = z.object({
+    label: z.string().min(1, 'Validation.labelMin').max(50, 'Validation.labelMax'),
+    url: z.url('Validation.invalidUrl'),
+});
+
+export const LinkSchema = LinkBaseSchema.extend({
+    taskId: z.string(),
+});
+
+export type LinkData = z.infer<typeof LinkSchema>;
