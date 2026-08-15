d97f69cc8e430dcf654faa7c0cb38c25b68b4343
Jakub Pavelka <pavelka.kuba@hotmail.cz>
2026-08-15 15:11:32 +0200

    Updated TaskDetailTags

--- app/frontend/src/components/features/Task/Detail/TaskDetailTags/TaskDetailTags.tsx
+++ app/frontend/src/components/features/Task/Detail/TaskDetailTags/TaskDetailTags.tsx
@@ -1,13 +1,17 @@
 import { Pill } from '@/components/ui/Pill/Pill';
-import { Tag } from 'lucide-react';
+import { Plus, Tag } from 'lucide-react';
 import { useTranslation } from 'react-i18next';
-import { useState } from 'react';
+import { useCallback, useState, type KeyboardEvent } from 'react';
 import { toast } from 'sonner';
 import { Popover } from '@/components/ui/Popover/Popover';
 import { useTagStore } from '@/store/tagStore';
 import { Checkbox } from '@/components/ui/Checkbox/Checkbox';
 import { useUpdateTask } from '@/hooks/api/useTask';
 import { ApiError } from '@/api/ApiError';
+import { LabelColorForm } from '@/components/features/Forms/LabelColorForm/LabelColorForm';
+import { TagSchema, type TagData } from '@timedo/shared/src/schemas/tagsSchema';
+import { useCreateTag } from '@/hooks/api/useTag';
+import clsx from 'clsx';
 import styles from './TaskDetailTags.module.scss';
 
 type TaskDetailTagsProps = {
@@ -21,11 +25,25 @@ type TaskDetailTagsProps = {
 
 export const TaskDetailTags = (props: TaskDetailTagsProps) => {
     const { t } = useTranslation();
-    const allTags = useTagStore((s) => s.tags);
     const { mutate: updateTask } = useUpdateTask(props.taskId);
+    const { mutate: createTag } = useCreateTag();
+    const allTags = useTagStore((s) => s.tags);
     const [showCreateTag, setShowCreateTag] = useState(false);
 
     const isTagChecked = (tagId: string) => props.tags.some((tag) => tag.id === tagId);
+    const handleShowCreateTag = () => setShowCreateTag(true);
+    const handleHideCreateTag = () => setShowCreateTag(false);
+    const handlePopoverOpenChange = useCallback((isOpen: boolean) => {
+        if (!isOpen) {
+            setShowCreateTag(false);
+        }
+    }, []);
+    const handleCreateTagKeyDown = (e: KeyboardEvent<HTMLDivElement>) => {
+        if (e.key === 'Enter' || e.key === ' ') {
+            e.preventDefault();
+            handleShowCreateTag();
+        }
+    };
 
     const handleOnCheckboxChange = (tagId: string) => {
         const tagIds = isTagChecked(tagId)
@@ -49,9 +67,26 @@ export const TaskDetailTags = (props: TaskDetailTagsProps) => {
         );
     };
 
+    const handleCreateTag = (data: TagData) => {
+        return createTag(data, {
+            onSuccess: () => {
+                toast.success(t('Task.Modal.createTagSuccess'));
+                setShowCreateTag(false);
+            },
+            onError: (err) => {
+                toast.error(
+                    err instanceof ApiError && err.code !== 'UNKNOWN_ERROR'
+                        ? t(`BackendErrors.${err.code}`)
+                        : t('Task.Modal.createTagError')
+                );
+            },
+        });
+    };
+
     return (
         <div className={styles.TaskDetailTags}>
             <Popover
+                onOpenChange={handlePopoverOpenChange}
                 trigger={
                     <div className={styles.TaskDetailTags__tagIconWrapper}>
                         <div className={styles.TaskDetailTags__tagIcon}>
@@ -61,23 +96,57 @@ export const TaskDetailTags = (props: TaskDetailTagsProps) => {
                     </div>
                 }
             >
-                <div className={styles.TaskDetailTags__popover}>
-                    {allTags.map((tag) => (
-                        <Checkbox
-                            key={tag.id}
-                            checked={isTagChecked(tag.id)}
-                            onChange={() => handleOnCheckboxChange(tag.id)}
-                            label={
-                                <div className={styles.TaskDetailTags__checkboxWrapper}>
-                                    <span
-                                        className={styles.TaskDetailTags__dot}
-                                        style={{ color: tag.color }}
-                                    />
-                                    {tag.label}
-                                </div>
-                            }
+                <div
+                    className={clsx(
+                        styles.TaskDetailTags__popover,
+                        showCreateTag && styles['TaskDetailTags__popover--createActive']
+                    )}
+                >
+                    {showCreateTag ? (
+                        <LabelColorForm
+                            schema={TagSchema}
+                            namePlaceholder={t('Task.Modal.tagName')}
+                            onSubmit={handleCreateTag}
+                            onClose={handleHideCreateTag}
                         />
-                    ))}
+                    ) : allTags.length > 0 ? (
+                        allTags.map((tag) => (
+                            <Checkbox
+                                key={tag.id}
+                                checked={isTagChecked(tag.id)}
+                                onChange={() => handleOnCheckboxChange(tag.id)}
+                                label={
+                                    <div
+                                        className={styles.TaskDetailTags__checkboxWrapper}
+                                    >
+                                        <span
+                                            className={styles.TaskDetailTags__dot}
+                                            style={{ color: tag.color }}
+                                        />
+                                        {tag.label}
+                                    </div>
+                                }
+                            />
+                        ))
+                    ) : (
+                        <p className={styles.TaskDetailTags__noTagsText}>
+                            {t('Task.Modal.noTags')}
+                        </p>
+                    )}
+                    {!showCreateTag && (
+                        <div
+                            onClick={handleShowCreateTag}
+                            onKeyDown={handleCreateTagKeyDown}
+                            role={'button'}
+                            tabIndex={0}
+                            className={styles.TaskDetailTags__createTagWrapper}
+                        >
+                            <p className={styles.TaskDetailTags__createTagText}>
+                                {t('Task.Modal.createTag')}
+                            </p>
+                            <Plus width={16} height={16} />
+                        </div>
+                    )}
                 </div>
             </Popover>
 
