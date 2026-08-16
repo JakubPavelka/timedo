ccad4f40a8ddebfa203c733348da062a0de1319c
Jakub Pavelka <pavelka.kuba@hotmail.cz>
2026-08-16 18:59:40 +0200

    Added round checkbox to TaskListItem

--- app/frontend/src/components/features/Task/TaskListItem/TaskListItem.tsx
+++ app/frontend/src/components/features/Task/TaskListItem/TaskListItem.tsx
@@ -1,6 +1,7 @@
 import { Priority } from '@timedo/shared/src/schemas/taskSchema';
 import { PriorityIcon } from '@/components/ui/PriorityIcon/PriorityIcon';
 import { Pill } from '@/components/ui/Pill/Pill';
+import { Checkbox } from '@/components/ui/Checkbox/Checkbox';
 import { useTranslation } from 'react-i18next';
 import type { Tag } from '@/store/tagStore';
 import type { Project } from '@/store/projectStore';
@@ -20,24 +21,32 @@ export const TaskListItem = (props: TaskListItem) => {
     return (
         <div className={styles.TaskListItem}>
             <div className={styles.TaskListItem__leftWrapper}>
-                <span className={styles.TaskListItem__title}>{props.title}</span>
-                <div className={styles.TaskListItem__pillsWrapper}>
-                    {props.project && (
-                        <Pill variant={'basic'} color={props.project.color} dot>
-                            {props.project.label}
+                <Checkbox round />
+                <div className={styles.TaskListItem__contentWrapper}>
+                    <span className={styles.TaskListItem__title}>{props.title}</span>
+                    <div className={styles.TaskListItem__pillsWrapper}>
+                        {props.project && (
+                            <Pill variant={'basic'} color={props.project.color} dot>
+                                {props.project.label}
+                            </Pill>
+                        )}
+                        {props.tags?.map((tag) => (
+                            <Pill
+                                key={tag.id}
+                                hashtag
+                                variant={'colored'}
+                                color={tag.color}
+                            >
+                                {tag.label}
+                            </Pill>
+                        ))}
+                        <Pill>
+                            <span className={styles.TaskListItem__priorityPill}>
+                                <PriorityIcon level={props.priority} />
+                                <span>{t(`Task.Priority.${priority}`)}</span>
+                            </span>
                         </Pill>
-                    )}
-                    {props.tags?.map((tag) => (
-                        <Pill key={tag.id} hashtag variant={'colored'} color={tag.color}>
-                            {tag.label}
-                        </Pill>
-                    ))}
-                    <Pill>
-                        <span className={styles.TaskListItem__priorityPill}>
-                            <PriorityIcon level={props.priority} />
-                            <span>{t(`Task.Priority.${priority}`)}</span>
-                        </span>
-                    </Pill>
+                    </div>
                 </div>
             </div>
         </div>
