0b26b34e9bdc7850a97864801cae9a3a429b6833
Jakub Pavelka <pavelka.kuba@hotmail.cz>
2026-08-20 21:33:59 +0200

    Updated view component

--- app/frontend/src/routes/dashboard/focus.tsx
+++ app/frontend/src/routes/dashboard/focus.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { DashboardView } from '@/views/Dashboard/DashboardView';
+import { FocusView } from '@/views/Focus/FocusView';
 
 export const Route = createFileRoute('/dashboard/focus')({
-    component: DashboardView,
+    component: FocusView,
 });
