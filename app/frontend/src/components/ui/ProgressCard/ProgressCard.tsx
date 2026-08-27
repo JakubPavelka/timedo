559ee47f1c77f93841a6824c3b6c8d61786cf03e
Jakub Pavelka <pavelka.kuba@hotmail.cz>
2026-08-27 17:34:01 +0200

    Added ProgressCard and AddCard components

---
+++ app/frontend/src/components/ui/ProgressCard/ProgressCard.tsx
@@ -0,0 +1,55 @@
+import { useTranslation } from 'react-i18next';
+import { Pencil } from 'lucide-react';
+import { formatDuration } from '@/utils/formatDuration';
+import styles from './ProgressCard.module.scss';
+
+type ProgressCardProps = {
+    title: string;
+    color: string;
+    tasksDone?: number;
+    totalTasks?: number;
+    duration?: number;
+    onEditClick?: () => void;
+    onClick?: () => void;
+};
+
+export const ProgressCard = (props: ProgressCardProps) => {
+    const { t } = useTranslation();
+
+    const tasksDone = props.tasksDone ?? 0;
+    const totalTasks = props.totalTasks ?? 0;
+    const progressPercent = totalTasks > 0 ? (tasksDone / totalTasks) * 100 : 0;
+
+    return (
+        <div className={styles.ProgressCard} onClick={props.onClick}>
+            <div className={styles.ProgressCard__topWrapper}>
+                <div className={styles.ProgressCard__titleWrapper}>
+                    <span
+                        className={styles.ProgressCard__dot}
+                        style={{ backgroundColor: props.color }}
+                    />
+                    <p className={styles.ProgressCard__title}>{props.title}</p>
+                </div>
+                <span onClick={props.onEditClick} className={styles.ProgressCard__edit}>
+                    <Pencil width={16} height={16} />
+                </span>
+            </div>
+            <div className={styles.ProgressCard__progressTrack}>
+                <div
+                    className={styles.ProgressCard__progressBar}
+                    style={{ width: `${progressPercent}%`, backgroundColor: props.color }}
+                />
+            </div>
+            <div className={styles.ProgressCard__bottomWrapper}>
+                <p className={styles.ProgressCard__tasks}>
+                    {t('ProgressCard.done', { tasksDone, allTasks: totalTasks })}
+                </p>
+                {!!props.duration && (
+                    <p className={styles.ProgressCard__time}>
+                        {formatDuration(props.duration)}
+                    </p>
+                )}
+            </div>
+        </div>
+    );
+};
